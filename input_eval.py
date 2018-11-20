3a32913d999d2ad8d28b6d044e0c3d767e452a6f
zongtong009 <zongtongguilai1@gmail.com>
2018-11-21 02:25:49 +0800

    Update input_eval.py

--- input_eval.py
+++ input_eval.py
@@ -5,8 +5,10 @@ while True:
         print(b)
         print(type(b))
     except:
-
         if a == '' or a == ' ':
             break
         else:
             print(a, '\nstr')
+'''input为int或float型时，可以直接用eval，
+为str时则应加' '，或直接不加eval，input本身为str型
+'''
