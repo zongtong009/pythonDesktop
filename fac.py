3b8305846ec96685bd4eaff05fc37cbd78a282e5
zongtong009 <zongtongguilai1@gmail.com>
2018-11-29 17:20:16 +0800

    update

--- fac.py
+++ fac.py
@@ -5,19 +5,19 @@ Created on Fri Sep 21 01:55:14 2018
 计算叠乘最后一位非0数位
 @author: Administrator
 """
-def fac(n):
-    sum=1
-    for i in range(1,n+1):       
-        sum*=i
-        while sum>=10 and sum%10==0:
-            sum=int(sum/10)        
-        sum=sum%10        
-    return sum
-    
-    
 
-n=eval(input())
-for i in range(1,n+1):
-    s=fac(i)
+
+def fac(n):
+    sum = 1
+    for i in range(1, n + 1):
+        sum *= i
+        while sum >= 10 and sum % 10 == 0:
+            sum = int(sum / 10)
+        sum = sum % 10
+    return sum
+
+
+n = eval(input())
+for i in range(1, n + 1):
+    s = fac(i)
     print(s)
-    
